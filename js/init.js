772bbeaa742c3ba88a25bbbbebf46d99e39da525
gerlis18 <gerlis1997@gmail.com>
2016-10-26 16:50:37 -0500

    Mi primer commit

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.dropdown-button').dropdown();
+    //$('.carousel.carousel-slider').carousel({full_width: true});
+     $('.tooltipped').tooltip({delay: 50});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
