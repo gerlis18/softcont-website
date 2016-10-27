c0944fc791af0b30a6c92aeb3b9c07b0a4c3bdd1
gerlis18 <gerlis1997@gmail.com>
2016-10-27 15:48:10 -0500

    Cambios en la pagina principal, se quito banner parallax y se añadio en la misma imagen fullscreen

--- js/init.js
+++ js/init.js
@@ -6,6 +6,8 @@
     $('.dropdown-button').dropdown();
     //$('.carousel.carousel-slider').carousel({full_width: true});
      $('.tooltipped').tooltip({delay: 50});
-
+     $('.slider').slider({
+         indicators: false
+     });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
