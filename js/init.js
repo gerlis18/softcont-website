eb82e96d247faacdc74859af3c9e320d75dc45b6
gerlis18 <gerlis1997@gmail.com>
2016-10-28 16:00:54 -0500

    Cambios en pagina principal, se modifico la seccion habla con nosotros y se creo una nueva vista contactenos

--- js/init.js
+++ js/init.js
@@ -1,13 +1,15 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    $('.dropdown-button').dropdown();
-    //$('.carousel.carousel-slider').carousel({full_width: true});
-     $('.tooltipped').tooltip({delay: 50});
-     $('.slider').slider({
-         indicators: false
-     });
-  }); // end of document ready
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('.dropdown-button').dropdown();
+        //$('.carousel.carousel-slider').carousel({full_width: true});
+        $('.tooltipped').tooltip({
+            delay: 50
+        });
+        $('.slider').slider({
+            indicators: false
+        });
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
